537c552916787bb75b388cad02a6ee9e18f5c1bc
Alex <alexchurilov@mail.ru>
2022-06-01 16:56:33 +0300

    Add Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа принимает с клавиатуры M чисел.
+// И считает сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через пробел: ");
+int [] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+}
+
+Console.WriteLine($"Количество введенных чисел больше 0 -> {count}");
